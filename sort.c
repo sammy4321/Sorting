2f9fd64b984e5e0f2393a82cd8ff80362660648a
sammy4321 <samarth.m.gowda@gmail.com>
2017-05-07 08:25:31 -0400

    Changes made to sort.c

--- sort.c
+++ sort.c
@@ -8,7 +8,7 @@ void merging(int low, int mid, int high) {
    int l1, l2, i;
 
    for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
-      if(a[l1] <= a[l2])
+      if(a[l1] >= a[l2])
          b[i] = a[l1++];
       else
          b[i] = a[l2++];
